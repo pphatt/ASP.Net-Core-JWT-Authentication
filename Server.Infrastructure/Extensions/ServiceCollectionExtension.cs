7a5d66eafdfddfb0bb3ad3a19c6ec75c2bcba9f4
pphatt <vutienphat0809@gmail.com>
2025-01-26 21:11:57 +0700

    feat: add jwt token generator

--- Server.Infrastructure/Extensions/ServiceCollectionExtension.cs
+++ Server.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Server.Application.Common.Interfaces.Authentication;
+using Server.Application.Common.Interfaces.Services;
+using Server.Infrastructure.Authentication;
 using Server.Infrastructure.Persistence;
+using Server.Infrastructure.Services;
 
 namespace Server.Infrastructure.Extensions;
 
@@ -9,7 +13,8 @@ public static class ServiceCollectionExtension
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDatabase(configuration);
+        services.AddDatabase(configuration)
+            .AddAuth(configuration);
 
         return services;
     }
@@ -19,6 +24,16 @@ public static class ServiceCollectionExtension
         services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
             .EnableSensitiveDataLogging());
+
+        return services;
+    }
+
+    public static IServiceCollection AddAuth(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SelectionName));
+
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 
         return services;
     }

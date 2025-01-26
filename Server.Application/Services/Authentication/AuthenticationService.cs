f30711f3e005b961158d17e0aeb1e9a0c331a0f7
pphatt <vutienphat0809@gmail.com>
2025-01-26 18:50:10 +0700

    feat: add basic authentication

---
+++ Server.Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿namespace Server.Application.Services.Authentication;
+
+public class AuthenticationService : IAuthenticationService
+{
+    public AuthenticationResult Login(string Email, string Password)
+    {
+        return new AuthenticationResult(Guid.NewGuid(), "FirstName", "LastName", Email, "Access Token");
+    }
+
+    public AuthenticationResult Register(string FirstName, string LastName, string Email, string Password)
+    {
+        return new AuthenticationResult(Guid.NewGuid(), FirstName, LastName, Email, "Access Token");
+    }
+}
